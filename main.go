f34ce5bd8f93440b1c469821c73ea0da805b67fd
Timothy C. Lam <geroginalam@hotmail.com>
2020-02-23 17:43:38 +0800

    db examples

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ import (
 	"unsafe"
 
 	"github.com/HouzuoGuo/tiedot/db"
+	"github.com/HouzuoGuo/tiedot/dberr"
 	"github.com/gorilla/websocket"
 
 	"github.com/ttimt/systray"
@@ -263,6 +264,79 @@ func onReady() {
 
 	// Setup system tray icon
 	setupTrayIcon()
+
+	// Test DB
+	testDB()
+}
+
+func testDB() {
+	// Create two collections: Feeds and Votes
+	if err := myDB.Create("Feeds"); err != nil {
+		panic(err)
+	}
+
+	// if err := myDB.Create("Votes"); err != nil {
+	// 	panic(err)
+	// }
+
+	// What collections do I now have?
+	// for _, name := range myDB.AllCols() {
+	// 	fmt.Printf("I have a collection called %s\n", name)
+	// }
+
+	// Rename collection "Votes" to "Points"
+	// if err := myDB.Rename("Votes", "Points"); err != nil {
+	// 	panic(err)
+	// }
+
+	// Drop (delete) collection "Points"
+	// if err := myDB.Drop("Points"); err != nil {
+	// 	panic(err)
+	// }
+
+	// Start using a collection (the reference is valid until DB schema changes or Scrub is carried out)
+	feeds := myDB.Use("Feeds")
+
+	// Insert document (afterwards the docID uniquely identifies the document and will never change)
+	docID, err := feeds.Insert(map[string]interface{}{
+		"name": "Go 1.2 is released",
+		"url":  "golang.org"})
+	if err != nil {
+		panic(err)
+	}
+
+	// Read document
+	readBack, err := feeds.Read(docID)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Document", docID, "is", readBack)
+
+	// Update document
+	err = feeds.Update(docID, map[string]interface{}{
+		"name": "Go is very popular",
+		"url":  "google.com"})
+	if err != nil {
+		panic(err)
+	}
+
+	// Process all documents (note that document order is undetermined)
+	feeds.ForEachDoc(func(id int, docContent []byte) (willMoveOn bool) {
+		fmt.Println("For each document", id, "is", string(docContent))
+		return true // move on to the next document OR
+		// return false // do not move on to the next document
+	})
+
+	// More complicated error handing - identify the error Type.
+	// In this example, the error code tells that the document no longer exists.
+	if err := feeds.Delete(docID); dberr.Type(err) == dberr.ErrorNoDoc {
+		fmt.Println("The document was already deleted")
+	}
+
+	// Drop (delete) collection "Feeds"
+	if err := myDB.Drop("Feeds"); err != nil {
+		panic(err)
+	}
 }
 
 func setupHTTPServer() {
